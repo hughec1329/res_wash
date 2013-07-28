155f3fb42e219bd75317fbc463cffc328ba1e0eb
hughec1329 <hughcrockford@gmail.com>
2013-07-28 12:35:04 +1000

    worked out stripper

---
+++ stripper.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cat $1 | awk '/Date/ {print $2}' >> out
+cat $1 | sed -n 7,30p | awk '{print $3}' >> out
+cat $1 | sed -n 35,38p | awk '{gsub(/%/,"",$3) ;print $3}'>> out
+echo '' >> out
